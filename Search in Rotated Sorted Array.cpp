aefabe72145555e198c4787d04360bb3c5dc34b9
Hongbo Liu <liuhb86@gmail.com>
2014-05-03 22:20:01 +0800

    2014 May

--- Search in Rotated Sorted Array.cpp
+++ Search in Rotated Sorted Array.cpp
@@ -1,30 +1,35 @@
-class Solution {
+﻿class Solution {
 public:
     int search(int A[], int n, int target) {
         // Start typing your C/C++ solution below
         // DO NOT write int main() function
-        int *begin = A, *end=A+n;
-        int *m = end;
-        
-        while(begin<end){
-            if (end-begin == 1) {m=begin;break}
-            int middle = begin + (end-begin)/2;
-            if (*middle<*begin) {end =middle +1; begin = begin+1;}
-            else if (*middle>*(end-1)) {begin = middle+1;}
-            else break;
+        if (n==0) return -1;
+        int* head = searchHead(A, A+n);
+        int last = A[n-1];
+        int* result;
+        if (target == last) return (n-1);
+        else if (target < last){
+            result = find(head, A+(n-1), target);
+            if (result!=A+(n-1)) return result-A; else return -1;
+        }
+        else
+        {
+            result = find(A,head, target);
+            if (result!=head) return result-A; else return -1;
         }
         
-        if (target>=*A) {begin=A;end=m;}
-        else {begin=m;end=A+n;}
-        
-        while(begin<end) {
-            int* middle = begin +(end-begin)/2;
-            if (target == *middle) return middle -A;
-            else if (target>*middle) begin = middle +1;
-            else end = target;
-        }
-        
-        return -1;
     }
-    
+private:
+    int* searchHead(int* begin, int* end)
+    {
+        if (end <= begin) return end;
+        if (*begin <= *(end-1)) return begin;
+        int* middle = begin+(end-begin)/2;
+        if (*(middle-1) >= *begin){
+            return searchHead(middle, end);
+        }
+        else {
+            return searchHead(begin, middle);
+        }
+    }
 };
