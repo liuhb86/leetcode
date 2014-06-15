960f91fc04e7451e94db3d21b082e5ba4384c78a
Hongbo Liu <liuhb86@gmail.com>
2014-06-15 22:10:46 +0800

    2014 Jun 15

---
+++ Merge Sorted Array.cpp
@@ -0,0 +1,18 @@
+﻿class Solution {
+public:
+    void merge(int A[], int m, int B[], int n) {
+        int* p = A + m - 1;
+        int* q = B + n -1;
+        int* t = A + m + n -1;
+        while(p>=A && q>=B) {
+            if (*p > *q) {
+                *(t--) = *(p--);
+            } else {
+                *(t--) = *(q--);
+            }
+        }
+        while(q>=B) {
+            *(t--) = *(q--);
+        }
+    }
+};
