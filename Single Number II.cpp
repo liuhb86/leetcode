27b31fa2597ade201acac6061fdc81ec13db13cd
Hongbo Liu <liuhb86@gmail.com>
2014-10-10 17:52:44 +0800

    2014 Oct 10

--- Single Number II.cpp
+++ Single Number II.cpp
@@ -1,34 +1,16 @@
 class Solution {
 public:
     int singleNumber(int A[], int n) {
-        vector<int> num;
-        int sign = 0;
-        for (int i=0;i<n;++i) {
-            unsigned int c;
-            if (A[i]<0) {
-                sign = (sign+1)%3;
-                c=-A[i];
-            } else {
-                c=A[i];
+        int result = 0;
+        int mask = 1;
+        for (int i=0;i<sizeof(int)*8;++i) {
+            int s=0;
+            for (int j=0;j<n;++j) {
+                if (A[j]&mask) ++s;
             }
-            int k=0;
-            while(c>0) {
-                int r = c%3;
-                c = c/3;
-                if (k>=num.size()) {
-                    num.push_back(r);
-                } else {
-                    num[k] = (num[k] + r) % 3;
-                }
-                ++k;
-            }
+            if (s%3) result= result|mask;
+            mask<<=1;
         }
-        
-        unsigned int result = 0;
-        for (vector<int>::reverse_iterator it=num.rbegin(); it!=num.rend();++it) {
-            result = result*3 + *it;
-        }
-        if (sign) result = -result;
         return result;
     }
 };
