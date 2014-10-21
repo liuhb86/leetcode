adf79569a38160ee6982efef101ee5f2d576d39f
Hongbo Liu <liuhb86@gmail.com>
2014-10-21 16:18:35 +0800

    pow

--- Pow(x, n).cpp
+++ Pow(x, n).cpp
@@ -1,6 +1,10 @@
 ﻿class Solution {
 public:
     double pow(double x, int n) {
-        return ::pow(x,n);
+        if (n==0) return (x==0)? NAN: 1;
+        double p = pow(x, n/2);
+        if (n%2==0) return p*p;
+        if (n>0) return p*p*x;
+        return p*p/x;
     }
 };
