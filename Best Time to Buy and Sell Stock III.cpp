aa54660fbb36ff343d2df3c971a3088162a91d87
Hongbo Liu <liuhb86@gmail.com>
2014-10-23 15:05:24 +0800

    buy stock

--- Best Time to Buy and Sell Stock III.cpp
+++ Best Time to Buy and Sell Stock III.cpp
@@ -1,56 +1,25 @@
 class Solution {
 public:
-    struct Range {
-        int buy;
-        int sell;
-        Range(int b, int s):buy(b), sell(s){}
-    };
     int maxProfit(vector<int> &prices) {
-        return maxProfit(prices, 2);
-    }
-    
-    int maxProfit(vector<int>& prices, int k) {
-        vector<Range> ranges;
-        Range r(0,0);
-	    int nPrices = prices.size();
-        for (int i = 0; i<nPrices -1; ++i) {
-            if (prices[i + 1] >= prices[i]) {
-                r.sell = i + 1;
-            } else {
-                if (r.sell > r.buy) ranges.push_back(r);
-                r.buy = i + 1;
-            }
+        if (prices.empty()) return 0;
+        vector<int> maxProfitSince(prices.size());
+        int maxPrice=prices.back();
+        int maxProfitEver = 0;
+        for (int i=prices.size()-1; i>=0;--i) {
+            maxPrice=max(maxPrice, prices[i]);
+            maxProfitEver = max(maxProfitEver, maxPrice-prices[i]);
+            maxProfitSince[i] = maxProfitEver;
         }
-        if (r.sell > r.buy) ranges.push_back(r);
         
-        if (ranges.empty()) return 0;
-        
-        int n = ranges.size();
-        
-        vector<vector<int> > profit(n);
-        vector<vector<int> > lastContIndex(n);
-        for (int i=0; i < n; ++i) {
-            profit[i].resize(k+1);
-            profit[i][0] = 0;
-            int p = prices[ranges[i].sell] - prices[ranges[i].buy];
-            if (i==0) {
-                for (int j=k; j>=1;--j) {
-                    profit[i][j] = p;
-                }
-                continue;
-            }
-            for (int j=k; j>=1; --j) {
-                int p1 = profit[i-1][j];
-                int p2 = profit[i-1][j -1] + p;
-                int maxp = max(p1,p2);
-                for (int r=0; r<i; ++r) {
-                    int p3 = (r ==0 ? 0 : profit[r-1][j-1]) + prices[ranges[i].sell] - prices[ranges[r].buy];
-                    if (p3 > maxp) maxp = p3;
-                }
-		        profit[i][j] = maxp;
-                
-            }
+        int result=0;
+        maxProfitEver =0;
+        int minPrice=prices.front();
+        for (int i=0;i<prices.size();++i) {
+            minPrice = min(minPrice, prices[i]);
+            maxProfitEver = max(maxProfitEver, prices[i]-minPrice);
+            result = max(result, maxProfitEver+maxProfitSince[i]);
         }
-        return profit[n-1][k];
+        
+        return result;
     }
 };
--- Best Time to Buy and Sell Stock.cpp
+++ Best Time to Buy and Sell Stock.cpp
@@ -1,16 +1,13 @@
 class Solution {
 public:
     int maxProfit(vector<int> &prices) {
-        int n = prices.size();
-        vector<int> delta(n);
-        for (int i = 0; i < n - 1; ++i) delta[i] = prices[i+1] - prices[i];
-        int maxprofit = 0;
-        int profit = 0;
-        for (int i=0; i < n-1; ++i) {
-            profit += delta[i];
-            if (profit < 0) profit = 0;
-            if (profit > maxprofit) maxprofit = profit;
+        if (prices.empty()) return 0;
+        int minPrice = prices[0];
+        int result = 0;
+        for (int i=0;i<prices.size();++i) {
+            result = max(result, prices[i]-minPrice);
+            minPrice=min(minPrice, prices[i]);
         }
-        return maxprofit;
+        return result;
     }
 };
