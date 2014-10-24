a22a64406a268a3170b60f3d612fc9f05a1f6755
Hongbo Liu <liuhb86@gmail.com>
2014-10-24 12:10:57 +0800

    refine

--- Regular Expression Matching.cpp
+++ Regular Expression Matching.cpp
@@ -1,35 +1,18 @@
+//Ref:
 class Solution {
 public:
     bool isMatch(const char *s, const char *p) {
-        int lenRegex=strlen(p);
-        set<int> loc1;
-        set<int> loc2;
-        set<int>* loc = &loc1;
-        set<int>* locNext = &loc2;
-        loc->insert(0);
-        for(;;++s){
-            if(loc->empty()) return false;
-            locNext->clear();
-            for(set<int>::iterator it=loc->begin(); it!=loc->end();++it){
-                int i=*it;
-                while (i!=lenRegex && p[i+1]=='*') {
-                    if (p[i]=='.' || p[i]==*s){
-                        locNext->insert(i);
-                        locNext->insert(i+2);
-                    }
-                    i+=2;
-                }
-                if (p[i]=='.' || p[i]==*s){
-                    locNext->insert(i+1);
-                }
+        if (!*p && !*s) return true;
+        if (!*p) return false;
+        if (*(p+1)=='*') {
+            while(*p=='.'&& *s || *p==*s) {
+                if (isMatch(s,p+2)) return true;
+                ++s;
             }
-            set<int>* tmp = loc;
-            loc=locNext;
-            locNext = tmp;
-            if (*s=='\0') break;
+            return isMatch(s,p+2);
+        } else if (*p=='.'&&*s || *p==*s) {
+            return isMatch(s+1,p+1);
         }
-        if(loc->count(lenRegex+1)!=0) return true;
-        else return false;
-        
+        return false;
     }
 };
--- Rotate Image.cpp
+++ Rotate Image.cpp
@@ -1,47 +1,19 @@
-﻿#include<vector>
-#include<iostream>
-using namespace std;
-
+﻿//ref:
 class Solution {
 public:
     void rotate(vector<vector<int> > &matrix) {
-        int size = matrix.size();
-        int nRow = size/2;
-        int nCol = (size+1)/2;
-        int c = (size*2 -1)/2;
-        for (int i=0;i<nRow; ++i) {
-            for (int j=0;j<nCol;++j) {
-                int v;
-                int i1=(i<<1) -c, j1=(j<<1) - c;
-                for (int phase=0; phase<=4;++phase) {
-                    int* p = &(matrix[(i1+c)>>1][(j1+c)>>1]);
-                    int t= *p;
-                    *p = v;
-                    v = t;
-                    t = i1;
-                    i1 = j1;
-                    j1 = -t;
-                }
+        int n=matrix.size();
+        //transpose
+        for (int i=0;i<n;++i) {
+            for (int j=0;j<i;++j) {
+                swap(matrix[i][j], matrix[j][i]);
             }
         }
-    }
-    static void main()
-    {
-        int A[2][2] = {{1,2},{3,4}};
-        int n = 2;
-        vector<vector<int> > matrix;
-        matrix.resize(n);
+        //mirror vertically
         for (int i=0;i<n;++i) {
-            matrix[i].resize(n);
-            for (int j=0;j<n;++j) matrix[i][j] = A[i][j];
-        }
-        Solution s;
-        s.rotate(matrix);
-        for (int i=0;i<n;++i) {
-            for (int j=0;j<n;++j) {
-                cout<<matrix[i][j]<<' ';
+            for (int j=0;j<n/2;++j) {
+                swap(matrix[i][j], matrix[i][n-j-1]);
             }
-            cout<<endl;
         }
     }
 };
