a585d34ce1142ee0b4cf28951920ae87454d78ff
Hongbo Liu <liuhb86@gmail.com>
2014-11-06 11:12:38 +0800

    refine Median of two sorted arrays

--- Median Of Two Sorted Arrays.cpp
+++ Median Of Two Sorted Arrays.cpp
@@ -1,29 +1,25 @@
 class Solution {
 public:
-    int findK(int* aBegin, int* aEnd, int* bBegin, int* bEnd, int k) {
-        if (aBegin>=aEnd) return bBegin[k];
-        if (bBegin>=bEnd) return aBegin[k];
-        if (aEnd-aBegin<bEnd-bBegin){
-            int* tmp;
-            tmp=aBegin;aBegin=bBegin; bBegin=tmp;
-            tmp=aEnd; aEnd=bEnd; bEnd=tmp;
+    int findK(int*A, int m, int*B, int n, int k) {
+        if (m==0) return B[k];
+        if (n==0) return A[k];
+        if (k==0) return min(A[0],B[0]);
+        int i=k/2;
+        int j=k-i-1;
+        int a= i<m ? A[i] : max(A[m-1], B[n-1]);
+        int b= j<n ? B[j] : max(A[m-1], B[n-1]);
+        if (a==b) return a;
+        if (a<b) {
+            return findK(A+i+1, m-i-1, B, n, k-i-1);
+        } else {
+            return findK(A,m, B+j+1, n-j-1, k-j-1);
         }
-        int* aMiddle = aBegin+(aEnd-aBegin)/2;
-        int middle = *aMiddle;
-        int* bMiddle = upper_bound(bBegin,bEnd, middle);
-        int position = aMiddle-aBegin + (bMiddle-bBegin);
-        if (position ==k) return middle;
-        else if (position>k)
-            return findK(aBegin,aMiddle,bBegin,bMiddle,k);
-        else
-            return findK(aMiddle+1,aEnd,bMiddle,bEnd,k-position-1);
     }
     double findMedianSortedArrays(int A[], int m, int B[], int n) {
-        if ((m+n)%2==1) return findK(A,A+m,B,B+n,(m+n)/2);
-        else {
-            double m1=findK(A,A+m,B,B+n,(m+n)/2-1);
-            double m2 = findK(A,A+m,B,B+n,(m+n)/2);
-            return (m1+m2)/2;
+        if ((m+n)%2==0) {
+            return (findK(A, m,B,n, (m+n)/2-1) + findK(A,m,B,n,(m+n)/2))/2.0;
+        } else {
+            return findK(A, m, B, n, (m+n)/2);
         }
     }
 };
