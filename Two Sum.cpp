f39693601c36fcedefc299fab4b1da6130e026c7
Hongbo Liu <liuhb86@gmail.com>
2015-07-18 12:24:56 -0400

    reimplement with hashtable

--- Two Sum.cpp
+++ Two Sum.cpp
@@ -1,40 +1,18 @@
-struct Number {
-    Number(int number, int index){
-        this->number = number;
-        this->index = index;
-    }
-    int number;
-    int index;
-    bool operator < (const Number& that) const{
-        return this->number<that.number;
-    }
-};
 class Solution {
 public:
-    vector<int> twoSum(vector<int> &numbers, int target) {
-        // Start typing your C/C++ solution below
-        // DO NOT write int main() function
+    vector<int> twoSum(vector<int>& nums, int target) {
         vector<int> result;
-        vector<Number> sorted;
-        for(int i=0;i<numbers.size();++i){
-            sorted.push_back(Number(numbers[i],i+1));
+        unordered_map<int, int> hashtable;
+        for (int i = 0; i<nums.size(); ++i) {
+            hashtable[nums[i]] = i;
         }
-        sort(sorted.begin(), sorted.end());
-        for(int i=0,j = sorted.size()-1; i<j;){
-            int sum = sorted[i].number+sorted[j].number;
-            if(sum==target){
-                if(sorted[i].index<sorted[j].index){
-                    result.push_back(sorted[i].index);
-                    result.push_back(sorted[j].index);
-                } else {
-                    result.push_back(sorted[j].index);
-                    result.push_back(sorted[i].index);
-                }
+        
+        for (int i=0; i<nums.size(); ++i) {
+            auto lResultIt = hashtable.find(target - nums[i]);
+            if (lResultIt != hashtable.end() && lResultIt->second != i) {
+                result.push_back(i + 1);
+                result.push_back(lResultIt->second + 1);
                 break;
-            } else if (sum<target) {
-                ++i;
-            } else {
-                --j;
             }
         }
         return result;
