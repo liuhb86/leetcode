abcc1a2e5d5c68d18372d03fd79f24a4b19b76d0
Hongbo Liu <liuhb86@gmail.com>
2015-08-13 21:48:51 -0400

    revise

--- Jump Game II.cpp
+++ Jump Game II.cpp
@@ -1,45 +1,17 @@
-﻿#include <vector>
-#include <iostream>
-using namespace std;
-
 class Solution {
 public:
-    int jump(int A[], int n) {
-        if (n<=1) return 0;
-        vector<int> locations[2];
-        vector<bool> visited(n);
-        locations[0].reserve(n);
-        locations[1].reserve(n);
-        locations[0].push_back(0);
-        visited[0] = true;
-        int workspace = 0;
-        int target = n-1;
-
-        for(int minStep = 1;minStep <n; ++minStep)
-        {
-            vector<int>* lpCur = &locations[workspace];
-            vector<int>* lpNext = &locations[1-workspace];
-            lpNext->clear();
-            for(vector<int>::iterator it=lpCur->begin();it!=lpCur->end(); ++it)
-            {
-                int loc = *it;
-                int furthest = loc + A[loc];
-                if (furthest >= target) return minStep;
-                for (int j=furthest;j>loc;--j)
-                {
-                    if (visited[j]) continue;
-                    visited[j] = true;
-                    lpNext->push_back(j);
-                }
-            }
-            workspace = 1-workspace;
+    int jump(vector<int>& nums) {
+        if (nums.empty()) return 0;
+        int n = nums.size();
+        vector<int> minStep(n);
+        minStep[n-1] = 0;
+        for (int i = n-2; i >= 0; --i) {
+            int d = i + nums[i];
+            minStep[i] = n;
+            int m = (d>=n) ? 1 : minStep[d] + 1;
+            minStep[i] = m;
+            for (int j = i + 1; minStep[j]>m;++j) minStep[j] = m;
         }
-        return n-1;
-    }
-    static void main()
-    {
-        int A[]={1,2};
-        Solution s;
-        cout<<  s.jump(A,2);
+        return minStep[0];
     }
 };
